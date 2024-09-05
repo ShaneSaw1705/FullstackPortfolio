91ca235cdaabb2adbc1d7967c74e6cac6f622189
Shane Curtis <shanecurtis1705@gmail.com>
2024-09-05 16:11:16 +1200

    Just need projects really now. plus some final touches and links

--- components/contactform.tsx
+++ components/contactform.tsx
@@ -1,5 +1,3 @@
-import { handleForm } from "@/hooks/formsubmit"
-
 const ContactForm = () => {
   return (
     <div className="hero bg-base-200 w-full h-screen">
@@ -12,18 +10,21 @@ const ContactForm = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form action={handleForm} className="card-body">
+          <form action={async (form: FormData) => {
+            'use server'
+            console.log(form)
+          }} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
+              <input name="email" type="email" placeholder="email" className="input input-bordered" required />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Message</span>
               </label>
-              <input type="text" placeholder="Message" className="input input-bordered" required />
+              <input name="message" type="text" placeholder="Message" className="input input-bordered" required />
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-secondary">Send</button>
